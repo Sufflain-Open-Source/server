7d02bd402638688e1b5d85b9aeb1c8daadf6896c
Timofey Chuchkanov <crt0r.9@yahoo.com>
2021-07-13 17:29:23 +0300

    Add data selectors for timetable pages

--- src/blog-scraper.rkt
+++ src/blog-scraper.rkt
@@ -17,8 +17,10 @@
 
 #lang racket/base
 
-(require "config.rkt"
-         sxml)
+(require "scraper.rkt"
+         "config.rkt"
+         sxml
+         racket/string)
 
 (define BLOG-LIST-ELEMENT-XPATH
   "//div[@class=\"kris-blogitem-all\"]/ol/li//div[@class=\"item-body-h\"]//a")
@@ -29,6 +31,66 @@
 ;; It contains a title and a link of the blog post.
 ;; (blog-post string? string?)
 (struct blog-post [title link])
+
+;; select-lessons-data: tbody -> (listof (listof xexpr?))
+;; Select info about each lesson. The result is a list of <td>s.
+(define (select-lessons-data tbody)
+  (if (null? tbody)
+      null
+      (let*
+          ([NO-TITLES    ((sxml:modify (list "//tr[1]" 'delete)) tbody)]
+           [NO-TIME      (map (lambda (element) ; Remove the first cell—which contains time—from each row.
+                                ((sxml:modify (list "//td[1]" 'delete)) element)) NO-TITLES)]
+           [LESSONS-DATA ((sxpath '(// tr)) NO-TIME)]
+           [NO-TR        (for/list ([row LESSONS-DATA])
+                           ((sxpath '(// td)) row))])
+        (compose NO-TR))))
+
+;; compose: (listof list?) -> (listof list?)
+;; Create a list of lists where each element of the Nth list is the first element of each initial list.
+;; IMPORTANT: Inner lists must have an equal number of elements.
+(define (compose lst)
+  (if (null? lst)
+      null
+      (let ([ANY-INNER-LIST-NULL? (andmap null? lst)])
+        (if ANY-INNER-LIST-NULL?
+            null
+            (letrec ([compose-first-elements
+                      (lambda (lst)
+                        (if (null? lst)
+                            null
+                            (cons (car (car lst)) (compose-first-elements (cdr lst)))))]
+                     [FIRST-ELEMENTS/LIST    (compose-first-elements lst)]
+                     [REDUCED-LISTS          (map cdr lst)])
+              (cons FIRST-ELEMENTS/LIST
+                    (compose REDUCED-LISTS)))))))
+
+;; select-titles: xexpr -> title-list
+;; Select timetable titles with groups.
+;; Titles on the site are formatted inconsistently.
+;; All of them have one thing in common — a group id.
+(define (select-titles tbody)
+  (select-from-tbody tbody "//p/strong/text()" "\\S{1,2}\\d{2}-\\d{2}.*"))
+
+;; select-time: xexpr -> time-list
+;; Select the time when classes start and end.
+;; Time is formatted as hh \u2013 mm.
+(define (select-time tbody)
+  (select-from-tbody tbody "//tr/td/p" "\\d{2}.\\d{2}\\s\\&ndash;\\s\\d{2}.\\d{2}"))
+
+;; select-from-tbody: xexpr string? string? -> (listof string?)
+;; Select data from the provided <tbody>.
+(define (select-from-tbody tbody xpath regex)
+  (let*
+      ([SELECTED-BY-XPATH                ((txpath xpath) tbody)]
+       [SELECTED-BY-XPATH/LIST-OF-STRING (for/list ([element SELECTED-BY-XPATH])
+                                           (srl:sxml->html element))])
+    (map string-trim (regex-select SELECTED-BY-XPATH/LIST-OF-STRING regex))))
+
+;; select-tbodys: xexpr -> (listof xexpr)
+;; Select <tbody>s from the timetable page.
+(define (select-tbodys page)
+  ((txpath "//table[@border=\"1\"]/tbody") page))
 
 ;; select-blog-posts: xexpr -> (listof blog-post)
 ;; Select blog posts from the blog page SXML.
@@ -48,7 +110,8 @@
         (blog-post TITLE FULL-LINK)))))
 
 (module+ test
-  (require rackunit
+  (require "shared/mocks.rkt"
+           rackunit
            mock
            racket/function)
   
@@ -58,7 +121,7 @@
            (college-site "https://example.url" "" "" ""))))
   
   ;; Scraped from the blog page
-  (define BLOG-MOCK
+  (define EXAMPLE-BLOG
     '(div 
       (@ (class "kris-blogitem-tabs"))
       (div
@@ -86,6 +149,48 @@
                 "/elektronnye_servisy/blog/uchchast/raspisanie-zanyatiy-na-2-iyulya-2021-g"))
               "Расписание занятий на 2 июля 2021 г."))))))))))
   
+  (define EXAMPLE-TABLE
+    '(*TOP* 
+      (tbody
+       (tr
+        (td "СА21-19, ауд. 304б"))
+       (tr
+        (td (p "10.00 " (&ndash) " 11.00"))
+        (td (p "Lesson data")))
+       (tr
+        (td (p "11.15 " (&ndash) " 12.30"))
+        (td (p "Lesson data"))))))
+  
+  (test-case "select-lessons-data"
+             (check-equal? (select-lessons-data null) null)
+             (check-equal? (select-lessons-data EXAMPLE-TABLE) '(((td (p "Lesson data"))
+                                                                  (td (p "Lesson data"))))))
+  
+  (test-case "compose"
+             (check-equal? (compose (list (list 1 2 3) (list 1 2 3) (list 1 2 3)))
+                           (list (list 1 1 1) (list 2 2 2) (list 3 3 3)))
+             (check-equal? (compose null) null)
+             (check-equal? (compose (list null null null null)) null)
+             (check-equal? (compose (list (list "a" "c") (list "b" "d")))
+                           (list (list "a" "b") (list "c" "d"))))
+  
+  (check-equal? (select-titles EXAMPLE-TBODY) '("Э11-20 ауд.319"
+                                                "Ф11-20 ауд.505"
+                                                "Б11-20 ауд.501"
+                                                "ИБ11-20 ауд.305"
+                                                "ИБ12-20 ауд.202"
+                                                "СА11-20 ауд.302"
+                                                "СА12-20 ауд."
+                                                "БА11-20 ауд.301"
+                                                "БА12-20 ауд.312"))
+  
+  (check-equal? (select-time EXAMPLE-TBODY) '("09.00 &ndash; 10.30"
+                                              "11.00 &ndash; 12.30"
+                                              "12.50 &ndash; 14.20"
+                                              "14.30 &ndash; 16.00"))
+  
+  (check-equal? (select-tbodys EXAMPLE-TIMETABLE-PAGE) `(,EXAMPLE-TBODY))
+  
   (test-case "select-blog-posts"
              (define EXAMPLE-BLOG-POST
                (blog-post "Расписание занятий на 2 июля 2021 г."
@@ -96,11 +201,11 @@ uchchast/raspisanie-zanyatiy-na-2-iyulya-2021-g"))
                                                   #:get-college-site-info-mock COLLEGE-SITE-INFO-MOCK))
              (check-equal? (blog-post-title 
                             (car 
-                             (select-blog-posts BLOG-MOCK
+                             (select-blog-posts EXAMPLE-BLOG
                                                 #:get-college-site-info-mock COLLEGE-SITE-INFO-MOCK)))
                            (blog-post-title EXAMPLE-BLOG-POST))
              (check-equal? (blog-post-link 
                             (car 
-                             (select-blog-posts BLOG-MOCK
+                             (select-blog-posts EXAMPLE-BLOG
                                                 #:get-college-site-info-mock COLLEGE-SITE-INFO-MOCK)))
                            (blog-post-link EXAMPLE-BLOG-POST))))
--- src/shared/mocks.rkt
+++ src/shared/mocks.rkt
@@ -30,6 +30,845 @@
 (define EXAMPLE-JSEXPR
   (string->jsexpr EXAMPLE-JSEXPR/STRING))
 
+(define EXAMPLE-TBODY
+  '(tbody
+    "\r\n"
+    "\t\t"
+    (tr
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:64px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "время "))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:132px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (strong "Э11-20 ауд.319"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "Ф11-20 ауд.505"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (strong (& nbsp) (& nbsp) (& nbsp) (& nbsp) (& nbsp) (& nbsp) " Б11-20 ауд.501"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "ИБ11-20 ауд.305"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "ИБ12-20 ауд.202"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "СА11-20 ауд.302"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "СА12-20 ауд."))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:151px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "БА11-20 ауд.301"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:34px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (strong "БА12-20 ауд.312"))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t")
+    "\r\n"
+    "\t\t"
+    (tr
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:64px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp))
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "09.00 " (& ndash) " 10.30" (& nbsp))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:132px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Естествознание")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Астрономия")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "ОБЖ")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Родной язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Химия")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Человек и общество")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "История")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Вебинар.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:151px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Иностранный язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Физическая культура")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Спорт.зал")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t")
+    "\r\n"
+    "\t\t"
+    (tr
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:64px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "11.00 " (& ndash) " 12.30" (& nbsp))
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:132px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Литература")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Экономика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Естествознание")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Химия")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Родной язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Информатика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.404")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Математика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Вебинар.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:151px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Математика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Иностранный язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t")
+    "\r\n"
+    "\t\t"
+    (tr
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:64px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp) "12.50 " (& ndash) " 14.20")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:132px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Иностран.язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Сверчкова А.В Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Родной язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Иностранный язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Математика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Русский язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Математика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Физика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp) "Вебинар.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:151px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Информатика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.404")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:32px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Химия")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t")
+    "\r\n"
+    "\t\t"
+    (tr
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:64px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp) "14.30 " (& ndash) " 16.00")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:132px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Литература")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Информатика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.404")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Физика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:160px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Физика")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Родной язык")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Вебинар.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:151px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) (& nbsp))
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t\t"
+     (td
+      (@ (style "width:161px;height:39px;"))
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Литература")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Фамилия И. О.")
+      "\r\n"
+      "\r\n"
+      "\t\t\t"
+      (p (@ (align "center")) "Ауд.")
+      "\r\n"
+      "\t\t\t")
+     "\r\n"
+     "\t\t")
+    "\r\n"
+    "\t"))
+
+;; Scraped from the college site.
+(define EXAMPLE-TABLE
+  `(table
+    (@ (border "1") (cellpadding "0") (cellspacing "0") (width "1472"))
+    "\r\n"
+    "\t"
+    ,EXAMPLE-TBODY
+    "\r\n"))
+
+;; Scraped from the college site.
+(define EXAMPLE-TIMETABLE-PAGE
+  `(div
+    (@ (class "kris-post-item-txt"))
+    (p
+     (div
+      "\r\n"
+      (table
+       (@ (border "0") (cellpadding "0") (cellspacing "0") (style "width:1393px;") (width "1394"))
+       "\r\n"
+       "\t"
+       (tbody
+        "\r\n"
+        "\t\t"
+        (tr
+         "\r\n"
+         "\t\t\t"
+         (td
+          (@ (style "width:577px;"))
+          "\r\n"
+          "\t\t\t"
+          (p (@ (align "center")) "Государственное бюджетное профессиональное")
+          "\r\n"
+          "\r\n"
+          "\t\t\t"
+          (p (@ (align "center")) "образовательное учреждение" (& nbsp) " города Москвы")
+          "\r\n"
+          "\r\n"
+          "\t\t\t"
+          (p
+           (@ (align "center"))
+           (strong (& laquo) "МОСКОВСКИЙ КОЛЛЕДЖ БИЗНЕС " (& ndash) " ТЕХНОЛОГИЙ" (& raquo)))
+          "\r\n"
+          "\r\n"
+          "\t\t\t"
+          (p (@ (align "center")) "(ГБПОУ КБТ)")
+          "\r\n"
+          "\t\t\t")
+         "\r\n"
+         "\t\t\t"
+         (td (@ (style "width:321px;")) "\r\n" "\t\t\t" (p (& nbsp)) "\r\n" "\t\t\t")
+         "\r\n"
+         "\t\t\t"
+         (td
+          (@ (style "width:496px;"))
+          "\r\n"
+          "\t\t\t"
+          (p (@ (align "center")) "Изменения в расписании")
+          "\r\n"
+          "\r\n"
+          "\t\t\t"
+          (p (@ (align "center")) "на четверг, 29 апреля" (& nbsp) " 2021 г.")
+          "\r\n"
+          "\r\n"
+          "\t\t\t"
+          (p (@ (align "center")) "(верхняя неделя)")
+          "\r\n"
+          "\t\t\t")
+         "\r\n"
+         "\t\t")
+        "\r\n"
+        "\t")
+       "\r\n")
+      "\r\n"
+      "\r\n"
+      (p (& nbsp))
+      "\r\n"
+      "\r\n"
+      (p (& nbsp))
+      "\r\n"
+      "\r\n"))
+    ,EXAMPLE-TABLE
+    "\r\n"
+    "\r\n"
+    (p (& nbsp))
+    "\r\n"
+    "\r\n"))
+
 (define GET-CONFIG-MOCK 
   (mock #:behavior 
         (const
